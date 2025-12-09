f3a460317bc2e8d4ae76bd006bfcafc5c3905aa0
Pratik Rane <pratikrane0412@gmail.com>
2025-12-09 13:39:53 +0530

    clients updated

--- components/sections/Clients.tsx
+++ components/sections/Clients.tsx
@@ -3,11 +3,11 @@ import React from "react";
 export const Clients = () => {
   const clients = [
     "MOTOEARTH",
-    "SMEDREAM",
+    "BLUEHOST",
     "WORKWISE",
-    "JOULES",
+    "HOUSE OF KHICHDI",
     "PAPITA",
-    "FITXFATLOSS",
+    "BRAGPACKER",
     "NATVENTURE",
   ];
 
